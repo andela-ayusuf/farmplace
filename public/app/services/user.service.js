f8f1eef6767dae01e08aa351f59f457595177666
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2016-02-09 22:21:29 +0100

    fix(frontend): fix service routes

--- public/app/services/user.service.js
+++ public/app/services/user.service.js
@@ -2,13 +2,13 @@ angular.module('farmplace')
 	.factory('UserService', ['$http', 'baseUrl', function($http, baseUrl) {
 		var User = {};
 		User.signup = function(user) {
-			return $http.post(baseUrl + '/api/signup', user).then(function(res) {
+			return $http.post(baseUrl + '/api/user/signup', user).then(function(res) {
         return res;
       });
 		};
 
 		User.login = function(user) {
-      return $http.post(baseUrl + '/api/login', user).then(function(res) {
+      return $http.post(baseUrl + '/api/user/login', user).then(function(res) {
         return res;
       });
     };
