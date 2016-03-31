0ba0f75540ded17a76b916e435e73db269c1c07a
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2016-03-31 21:24:57 +0100

    refactor(backend): remove/add gulp plugins and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,36 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
-var cssmin = require('gulp-cssmin');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
 var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+var cache = require('gulp-cache');
 var del = require('del');
-var rename = require('gulp-rename');
 var nodemon = require('gulp-nodemon');
+var runSequence = require('run-sequence');
 
-var jsFiles = ['public/app/controllers/*.js', 'public/app/services/*.js', 'public/app/app.js'];
-var cssFiles = 'public/assets/css/*.css';
-// var imgFiles = 'public/assets/images/*';
 
-// minify js files
-gulp.task('scripts', function() {
-  return gulp.src(jsFiles)
-    .pipe(concat('main.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
-});
-
-// minify css files
-gulp.task('stylesheets', function () {
-  gulp.src(cssFiles)
-    .pipe(concat('main.css'))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/css'));
-});
-
-// minify images
-// gulp.task('images', function () {
-//   return gulp.src(imgFiles)
-//     .pipe(imagemin({
-//       progressive: true,
-//       svgoPlugins: [{removeViewBox: false}],
-//       use: [pngquant()]
-//     }))
-//     .pipe(gulp.dest('build/images'));
-// });
-
-// clean up
+// clean the build directory
 gulp.task('clean', function() {
-  return del(['build/js', 'build/css']);
+  return del.sync('build');
+});
+
+// minify css and js files
+gulp.task('minify', function() {
+  return gulp.src('public/index.html')
+  .pipe(useref())
+  .pipe(gulpIf('*.js', uglify()))
+  .pipe(gulpIf('*.css', cssnano()))
+  .pipe(gulp.dest('build'))
+});
+
+// optimize images
+gulp.task('imgMin', function(){
+  return gulp.src('public/assets/img/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('build/img'))
 });
 
 // start app
@@ -54,9 +41,7 @@ gulp.task('nodemon', function () {
   });
 });
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('scripts', 'stylesheets', 'nodemon');
+gulp.task('default', function() {
+  runSequence('clean', ['minify', 'imgMin'], 'nodemon')
 });
-
-
 
