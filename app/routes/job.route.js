b57b6aadb439711088f69373adf8945e24fc89d7
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2016-04-06 16:51:30 +0100

    refactor(backend): remove middleware protecting routes

--- app/routes/job.route.js
+++ app/routes/job.route.js
@@ -1,15 +1,14 @@
 var jobCtrl = require('../controllers/job.controller');
-var userCtrl = require('../controllers/user.controller');
 
 function jobRoutes(router) {
 	router.route('/jobs')
-		.post(userCtrl.middleware, jobCtrl.postJob)
-		.get(userCtrl.middleware, jobCtrl.getAllJobs);
+		.post(jobCtrl.postJob)
+		.get(jobCtrl.getAllJobs);
 
 	 router.route('/jobs/:id')
-    .get(userCtrl.middleware, jobCtrl.getJob)
-    .put(userCtrl.middleware, jobCtrl.editJob)
-    .delete(userCtrl.middleware, jobCtrl.deleteJob);
+    .get(jobCtrl.getJob)
+    .put(jobCtrl.editJob)
+    .delete(jobCtrl.deleteJob);
 }
 
 module.exports = jobRoutes;
