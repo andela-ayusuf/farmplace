2688dfa4e56ecc8834f4d15d909fd51e629319c9
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2016-05-08 16:43:03 +0100

    feat(backend): add handler for 404 paths

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,10 @@ function routes(router) {
   authRoutes(router);
   jobRoutes(router);
   farmOwnerRoutes(router);
+
+  router.use(function(req, res, next) {
+  	res.status(404).send({message: 'Sorry couldnt find that route!'});
+  });
 }
 
 module.exports = routes;
