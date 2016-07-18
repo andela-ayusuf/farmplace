cd6836629412e660870a71cf0b0433ef563ba6ea
andela-ayusuf <abdulmalik.yusuf@andela.com>
2016-07-18 23:16:19 +0100

    feat(backend): add application model

---
+++ app/models/application.model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var User = require('./user.model');
+var Job = require('./job.model');
+
+var applicationSchema = new Schema({
+	applicantId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	jobId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Job'
+	},
+	details: {
+		type: String
+	}
+});
+
+module.exports = mongoose.model('Application', applicationSchema)
