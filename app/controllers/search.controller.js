579fa24e0ced2e8c7d9e04ba184b5bccc58ef63e
andela-ayusuf <abdulmalik.yusuf@andela.com>
2016-08-27 18:57:42 +0100

    refactor(backend): modify search functionality

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -1,21 +1,70 @@
 var Job = require('../models/job.model');
 
 exports.search = function search(req, res) {
-	if (req.body === null) {
-		Job.find({})
+	var title = new RegExp(req.query.title, 'i');
+	var location = new RegExp(req.query.location, 'i');
+
+	if (req.query.location === undefined) {
+		Job.find({title: title})
 		.then(function(jobs) {
-			return res.status(200).send(jobs);
+			if (jobs.length === 0) {
+				return res.status(200).send({
+					success: true,
+          message: 'Your search for ' + req.query.title + ' returned no results.'
+				});
+			}
+			return res.status(200).send({
+				success: true,
+				message: 'Here are your search results',
+				results: jobs
+			});
 		})
-	}
-	Job.find({title: req.body.title, location: req.body.location, agricType: req.body.agricType})
-	.then(function(jobs) {
-		return res.status(200).send(jobs);
-	})
-	.catch(function(err) {
-		return res.status(403).send({
-			success: false,
-			message: 'Not found!',
-			error: err
+		.catch(function(err) {
+			return res.status(403).send({
+				success: false,
+				message: 'An error occured!',
+				error: err
+			});
 		});
-	});
+	}
+	else if (req.query.title === undefined) {
+		Job.find({location: location})
+		.then(function(jobs) {
+			if (jobs.length === 0) {
+				return res.status(200).send({
+					success: true,
+          message: 'Your search for ' + req.query.location + ' returned no results.'
+				});
+			}
+			return res.status(200).send({
+				success: true,
+				message: 'Here are your search results',
+				results: jobs
+			});
+		})
+		.catch(function(err) {
+			return res.status(403).send({
+				success: false,
+				message: 'An error occured!',
+				error: err
+			});
+		});
+	}
+	else {
+		Job.find({$or: [{title: title}, {location: location}]})
+		.then(function(jobs) {
+			return res.status(200).send({
+				success: true,
+				message: 'Here are your search results',
+				results: jobs
+			});
+		})
+		.catch(function(err) {
+			return res.status(403).send({
+				success: false,
+				message: 'An error occured!',
+				error: err
+			});
+		});
+	}
 };
