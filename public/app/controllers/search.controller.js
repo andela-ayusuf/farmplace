1b6b0303419a3953045f507a98e368855bf08a23
andela-ayusuf <abdulmalik.yusuf@andela.com>
2016-09-04 01:06:27 +0100

    refactor(frontend): modify search function

--- public/app/controllers/search.controller.js
+++ public/app/controllers/search.controller.js
@@ -1,9 +1,16 @@
 angular.module('farmplace')
-	.controller('SearchCtrl', ['$scope', 'SearchService', '$location', function($scope, SearchService, $location) {
+	.controller('SearchCtrl', ['$scope', 'SearchService', '$location', '$rootScope', function($scope, SearchService, $location, $rootScope) {
 
 		$scope.search = function() {
 			SearchService.search($scope.terms).then(function(res) {
-				$scope.results = res.data;
+        var noResults;
+        if (res.data.success === false) {
+          $rootScope.noResults = true;
+        }
+        else if (res.data.success === true) {
+        	$rootScope.noResults = false;
+        }
+				$rootScope.searchResults = res.data.results;
 				$location.url('/search');
 			}, function(err) {
 				console.log(err)
